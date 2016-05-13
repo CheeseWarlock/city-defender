14b18ff56363ff7fb7ba9e7a115a25a32b4b378a
Joe Cardwell <joe.cardwell@gmail.com>
2016-05-13 00:07:36 -0400

    Extract building generation and setup

--- script.js
+++ script.js
@@ -1,20 +1,91 @@
-var renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize( window.innerWidth - 1, window.innerHeight - 1 );
-document.body.appendChild( renderer.domElement );
+SceneManager = {
+	scene: null,
+	lights: [],
+
+	init: function() {
+		this.renderer = new THREE.WebGLRenderer({ antialias: true });
+		this.renderer.setSize(window.innerWidth - 1, window.innerHeight - 1);
+		document.body.appendChild(this.renderer.domElement);
+		this.scene = new THREE.Scene();
+	},
+
+	addLights: function() {
+		var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.4 );
+		directionalLight.position.set( 1, 3, 2 );
+		this.scene.add( directionalLight );
+
+		var light = new THREE.AmbientLight( 0xffffff, 0.6 ); // soft white light
+		this.scene.add( light );
+		this.scene.fog = new THREE.Fog( 0xFF7CD3, 10, 40 );
+	}
+}
+
+BuildingFactory = {
+	setup: function(scene) {
+		this.scene = scene;
+	},
+
+	makeBuilding: function(x, z, transparent) {
+		if (transparent) {
+			this.makeWallSet(x, z);
+			this.makeFloorSet(x, z);
+		} else {
+			this.makeWall(x + 4.5, z, Math.PI/2, loadedMaterials["exterior_solid"]);
+			this.makeWall(x - 4.5, z, -Math.PI/2, loadedMaterials["exterior_solid"]);
+			this.makeWall(x, z + 4.5, 0, loadedMaterials["exterior_solid"]);
+			this.makeWall(x, z - 4.5, Math.PI, loadedMaterials["exterior_solid"]);
+		}
+	},
+
+	makeWallSet: function(x, z) {
+		this.makeWall(x + 4.5, z, Math.PI/2, loadedMaterials["exterior"]);
+		this.makeWall(x - 4.5, z, -Math.PI/2, loadedMaterials["exterior"]);
+		this.makeWall(x, z + 4.5, 0, loadedMaterials["exterior"]);
+		this.makeWall(x, z - 4.5, Math.PI, loadedMaterials["exterior"]);
+		this.makeWall(x + 4.5, z, Math.PI/2, loadedMaterials["interior"]);
+		this.makeWall(x - 4.5, z, -Math.PI/2, loadedMaterials["interior"]);
+		this.makeWall(x, z + 4.5, 0, loadedMaterials["interior"]);
+		this.makeWall(x, z - 4.5, Math.PI, loadedMaterials["interior"]);
+		this.makeWall(x, z, 0, new THREE.MeshBasicMaterial({color: 0x4040404}));
+		this.makeWall(x, z, Math.PI, new THREE.MeshBasicMaterial({color: 0x4040404}));
+	},
+
+	makeWall: function(x, z, rotation, p) {
+		var wallGeometry = new THREE.PlaneGeometry(9, 36);
+		var wall = new THREE.Mesh(wallGeometry, p);
+		wall.rotation.y = rotation;
+		wall.position.x = x;
+		wall.position.z = z;
+		this.scene.add(wall);
+	},
+
+	makeFloorSet: function(x, z) {
+		var floorGeometry = new THREE.PlaneGeometry(9, 9);
+		for (var y=-10;y<=10;y++) {
+			var floor = new THREE.Mesh(floorGeometry, loadedMaterials[y > 0 ? "ceiling": "floor"]);
+			floor.position.x = x;
+			floor.position.y = y;
+			floor.position.z = z;
+			floor.rotation.x = Math.PI / 2;
+			this.scene.add(floor);
+		}
+	}
+}
+
+SceneManager.init();
+SceneManager.addLights();
+scene = SceneManager.scene;
+renderer = SceneManager.renderer;
+BuildingFactory.setup(scene);
+
 
 var buildings = [];
 var mirrors = [];
 var anim = 0;
 var groundMirror = false;
 
-scene = new THREE.Scene();
-var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.4 );
-directionalLight.position.set( 1, 3, 2 );
-scene.add( directionalLight );
 
-var light = new THREE.AmbientLight( 0xffffff, 0.6 ); // soft white light
-scene.add( light );
-scene.fog = new THREE.Fog( 0xFF7CD3, 10, 40 );
+
 
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 renderer.setClearColor( 0xFF7CD3, 1);
@@ -26,26 +97,27 @@ unloaded = 5;
 keys = {};
 offset = 0;
 
-var renderer2 = new THREE.WebGLRenderer();
-renderer2.setSize(200, 200);
-document.body.appendChild( renderer2.domElement );
-var scene2 = new THREE.Scene();
-var light2 = new THREE.AmbientLight( 0xffffff, 0.6 );
-scene2.add( light2 );
-var camera2 = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
-camera2.position.z = 20;
+// var renderer2 = new THREE.WebGLRenderer();
+// renderer2.setSize(200, 200);
+// document.body.appendChild( renderer2.domElement );
+// var scene2 = new THREE.Scene();
+// var light2 = new THREE.AmbientLight( 0xffffff, 0.6 );
+// scene2.add( light2 );
+// var camera2 = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
+// camera2.position.z = 20;
 
-var cc = new THREE.Mesh(new THREE.PlaneGeometry(10,10), new THREE.MeshBasicMaterial({color: 0x404040}));
+// var cc = new THREE.Mesh(new THREE.PlaneGeometry(10,10), new THREE.MeshBasicMaterial({color: 0x404040}));
 
-scene2.add(cc);
+// scene2.add(cc);
 
-var dd = new THREE.Mesh(new THREE.PlaneGeometry(1,100), new THREE.MeshBasicMaterial({color: 0xff0000}));
+// var dd = new THREE.Mesh(new THREE.PlaneGeometry(1,100), new THREE.MeshBasicMaterial({color: 0xff0000}));
 
-scene2.add(dd);
+// scene2.add(dd);
 
 loader.load("a.png", function(texture) {
 	loadedTextures["exterior"] = texture;
 	loadedMaterials["exterior"] = new THREE.MeshLambertMaterial( { map: texture, transparent: true, side: THREE.DoubleSide } );
+	loadedMaterials["exterior_solid"] = new THREE.MeshLambertMaterial( { map: texture, side: THREE.DoubleSide } );
 	texture.wrapS = THREE.RepeatWrapping;
 	texture.wrapT = THREE.RepeatWrapping;
 	texture.repeat.set( 9, 36 );
@@ -89,45 +161,10 @@ loader.load("fairy.png", function(texture) {
 	if (!--unloaded) loadingDone();
 });
 
-function makeWallSet(x, z) {
-	makeWall(x + 4.5, z, Math.PI/2, loadedMaterials["exterior"]);
-	makeWall(x - 4.5, z, -Math.PI/2, loadedMaterials["exterior"]);
-	makeWall(x, z + 4.5, 0, loadedMaterials["exterior"]);
-	makeWall(x, z - 4.5, Math.PI, loadedMaterials["exterior"]);
-	makeWall(x + 4.5, z, Math.PI/2, loadedMaterials["interior"]);
-	makeWall(x - 4.5, z, -Math.PI/2, loadedMaterials["interior"]);
-	makeWall(x, z + 4.5, 0, loadedMaterials["interior"]);
-	makeWall(x, z - 4.5, Math.PI, loadedMaterials["interior"]);
-	makeWall(x, z, 0, new THREE.MeshBasicMaterial({color: 0x4040404}));
-	makeWall(x, z, Math.PI, new THREE.MeshBasicMaterial({color: 0x4040404}));
-}
-
-function makeWall(x, z, rotation, p) {
-	var wallGeometry = new THREE.PlaneGeometry(9, 36);
-	var wall = new THREE.Mesh(wallGeometry, p);
-	wall.rotation.y = rotation;
-	wall.position.x = x;
-	wall.position.z = z;
-	scene.add(wall);
-}
-
-function makeFloorSet(x, z) {
-	var floorGeometry = new THREE.PlaneGeometry(9, 9);
-	for (var y=-10;y<=10;y++) {
-		var floor = new THREE.Mesh(floorGeometry, loadedMaterials[y > 0 ? "ceiling": "floor"]);
-		floor.position.x = x;
-		floor.position.y = y;
-		floor.position.z = z;
-		floor.rotation.x = Math.PI / 2;
-		scene.add(floor);
-	}
-}
-
 function loadingDone() {
 	for (var i=0;i<=5;i++) {
 		for (var j=0;j<=5;j++) {
-			makeWallSet(i * 12 - 24, j * 12 - 24);
-			makeFloorSet(i * 12 - 24, j * 12 - 24);
+			BuildingFactory.makeBuilding(i * 12 - 24, j * 12 - 24, false);
 		}
 	}
 
@@ -198,7 +235,7 @@ function render() {
 	//renderer.clear();
 	renderer.render( scene, camera );
 	//renderer.clearDepth();
-	renderer2.render(scene2, camera2);
+	//renderer2.render(scene2, camera2);
 }
 
 render();
