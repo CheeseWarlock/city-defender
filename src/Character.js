6ecbb0a3b1f116f66bf00f92c36de6bfd8037458
Joe Cardwell <joe.cardwell@gmail.com>
2016-05-23 22:42:02 -0400

    Sanitize position logic

--- src/Character.js
+++ src/Character.js
@@ -16,40 +16,49 @@ class Character {
 		this.direction = 0;
 	}
 
-	canTurn() {
-		return this.distance % 12 > BUILDING_WIDTH && this.distance % 12 < 12 - BUILDING_WIDTH - DISTANCE_BACK;
+	isTurning() {
+		return this.distance % 12 > BUILDING_WIDTH && this.distance % 12 < 12 - BUILDING_WIDTH - BUILDING_MARGIN;
 	}
 
 	update() {
-		this.direction = 0;
 		this.distance += MOVE_SPEED;
-		this.distance = this.distance % (12 - 1.5);
-		if (true) {
-			this.camera.rotation.y = ANGLING;
-			this.camera.position.x = this.distance;
-			this.camera.position.z = DISTANCE_BACK + BUILDING_WIDTH;
-		}
-		if (this.canTurn()) {
-			var turnProgress = (this.distance - BUILDING_WIDTH) / DISTANCE_BACK; // from 0 to 1
-			this.camera.rotation.y = turnProgress * Math.PI / 2 + ANGLING;
-			this.camera.position.x = BUILDING_WIDTH + DISTANCE_BACK * Math.sin(turnProgress * Math.PI / 2);
-			this.camera.position.z = BUILDING_WIDTH + DISTANCE_BACK * Math.cos(turnProgress * Math.PI / 2);
-		}
-		if (this.distance % 12 > 12 - BUILDING_WIDTH - DISTANCE_BACK) {
-			this.camera.rotation.y = Math.PI / 2 + ANGLING;
-			this.camera.position.x = BUILDING_WIDTH + DISTANCE_BACK;
-			this.camera.position.z = BUILDING_WIDTH + DISTANCE_BACK - this.distance + BUILDING_WIDTH;
+		if (this.distance > 10.5) {
+			this.distance %= 10.5;
+			this.direction = 0;
 		}
 
-		this.model.position.x = this.camera.position.x - Math.sin(this.camera.rotation.y) - offset * Math.cos(this.camera.rotation.y);
-		this.model.position.z = this.camera.position.z - Math.cos(this.camera.rotation.y) + offset * Math.sin(this.camera.rotation.y);
+		this.center = {
+			x: this.distance,
+			z: BUILDING_WIDTH + BUILDING_MARGIN
+		};
+		if (this.distance <= BUILDING_WIDTH) {
+
+		} else if (this.isTurning()) {
+			this.direction = (this.distance - BUILDING_WIDTH) / BUILDING_MARGIN; // from 0 to 1
+			this.center = {
+				x: BUILDING_WIDTH + BUILDING_MARGIN * Math.sin(this.direction * Math.PI / 2),
+				z: BUILDING_WIDTH + BUILDING_MARGIN * Math.cos(this.direction * Math.PI / 2)
+			}
+		} else if (this.distance % 12 > 12 - BUILDING_WIDTH - BUILDING_MARGIN) {
+			this.center = {
+				x: BUILDING_WIDTH + BUILDING_MARGIN,
+				z: BUILDING_WIDTH + BUILDING_MARGIN - this.distance + BUILDING_WIDTH
+			}
+		}
+
+		this.camera.position.x = this.center.x + CAMERA_DISTANCE_BACK * Math.sin(this.direction * Math.PI / 2);
+		this.camera.position.z = this.center.z + CAMERA_DISTANCE_BACK * Math.cos(this.direction * Math.PI / 2);
+		this.camera.rotation.y = this.direction * Math.PI / 2 + ANGLING;
+
+		this.model.position.x = this.center.x - offset * Math.cos(this.direction * Math.PI / 2);
+		this.model.position.z = this.center.z + offset * Math.sin(this.direction * Math.PI / 2);
 
 		var qq = this.distance + 0.2;
 		if (qq <= 5) {
 			this.model.rotation.y = 0;
 		}
-		if (qq > BUILDING_WIDTH && qq < BUILDING_WIDTH + DISTANCE_BACK) {
-			var q = (qq - BUILDING_WIDTH) / DISTANCE_BACK;
+		if (qq > BUILDING_WIDTH && qq < BUILDING_WIDTH + BUILDING_MARGIN) {
+			var q = (qq - BUILDING_WIDTH) / BUILDING_MARGIN;
 			this.model.rotation.y = q * Math.PI / 2;
 		}
 
