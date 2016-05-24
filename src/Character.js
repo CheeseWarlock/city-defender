b98b106bf6bd7eaa3352c89440be191f665d763b
Joe Cardwell <joe.cardwell@gmail.com>
2016-05-24 00:17:56 -0400

    f

--- src/Character.js
+++ src/Character.js
@@ -17,54 +17,55 @@ class Character {
 	}
 
 	isTurning() {
-		return this.distance % 12 > BUILDING_WIDTH && this.distance % 12 < 12 - BUILDING_WIDTH - BUILDING_MARGIN;
+		return this.distance % 12 > BUILDING_WIDTH && this.distance % 12 < 12 - BUILDING_WIDTH;
 	}
 
 	update() {
+		if (window.controller.pressed(controller.DOWN)) this.model.position.y -= 0.01;
+		if (window.controller.pressed(controller.UP)) this.model.position.y += 0.01;
+		if (window.controller.pressed(controller.RIGHT)) offset += 0.01;
+		if (window.controller.pressed(controller.LEFT)) offset -= 0.01;
+
 		this.distance += MOVE_SPEED;
 		if (this.distance > 10.5) {
-			this.distance %= 10.5;
-			this.direction = 0;
+
 		}
 
-		this.center = {
-			x: this.distance,
-			z: BUILDING_WIDTH + BUILDING_MARGIN
-		};
 		if (this.distance <= BUILDING_WIDTH) {
-
-		} else if (this.isTurning()) {
-			this.direction = (this.distance - BUILDING_WIDTH) / BUILDING_MARGIN; // from 0 to 1
 			this.center = {
-				x: BUILDING_WIDTH + BUILDING_MARGIN * Math.sin(this.direction * Math.PI / 2),
-				z: BUILDING_WIDTH + BUILDING_MARGIN * Math.cos(this.direction * Math.PI / 2)
+				x: this.distance,
+				z: BUILDING_WIDTH + BUILDING_MARGIN
+			};
+		} else if (this.isTurning()) {
+			if (!this.pivot) {
+				this.pivot = {
+					x: BUILDING_WIDTH,
+					z: BUILDING_WIDTH
+				};
 			}
-		} else if (this.distance % 12 > 12 - BUILDING_WIDTH - BUILDING_MARGIN) {
+			this.direction = (this.distance - BUILDING_WIDTH) / BUILDING_MARGIN / 2;
+			this.center = {
+				x: this.pivot.x + BUILDING_MARGIN * Math.sin(this.direction * Math.PI / 2),
+				z: this.pivot.z + BUILDING_MARGIN * Math.cos(this.direction * Math.PI / 2)
+			}
+		} else if (this.distance > 12 - BUILDING_WIDTH - BUILDING_MARGIN) {
 			this.center = {
 				x: BUILDING_WIDTH + BUILDING_MARGIN,
-				z: BUILDING_WIDTH + BUILDING_MARGIN - this.distance + BUILDING_WIDTH
+				z: BUILDING_MARGIN - BUILDING_WIDTH - this.distance + 15
 			}
 		}
 
 		this.camera.position.x = this.center.x + CAMERA_DISTANCE_BACK * Math.sin(this.direction * Math.PI / 2);
 		this.camera.position.z = this.center.z + CAMERA_DISTANCE_BACK * Math.cos(this.direction * Math.PI / 2);
 		this.camera.rotation.y = this.direction * Math.PI / 2 + ANGLING;
+		// this.camera.position.y = 10;
+		// this.camera.rotation.x = -1;
+		// this.camera.position.x = 5;
+		// this.camera.position.z = 6;
 
 		this.model.position.x = this.center.x - offset * Math.cos(this.direction * Math.PI / 2);
 		this.model.position.z = this.center.z + offset * Math.sin(this.direction * Math.PI / 2);
 
-		var qq = this.distance + 0.2;
-		if (qq <= 5) {
-			this.model.rotation.y = 0;
-		}
-		if (qq > BUILDING_WIDTH && qq < BUILDING_WIDTH + BUILDING_MARGIN) {
-			var q = (qq - BUILDING_WIDTH) / BUILDING_MARGIN;
-			this.model.rotation.y = q * Math.PI / 2;
-		}
-
-		if (window.controller.pressed(controller.DOWN)) this.model.position.y -= 0.01;
-		if (window.controller.pressed(controller.UP)) this.model.position.y += 0.01;
-		if (window.controller.pressed(controller.RIGHT)) offset += 0.01;
-		if (window.controller.pressed(controller.LEFT)) offset -= 0.01;
+		this.model.rotation.y = this.direction * Math.PI / 2;
 	}
 }
