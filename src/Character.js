a44f6cf544e4c59d2efcfe3f54de7db912eaff92
Joe Cardwell <joe.cardwell@gmail.com>
2016-05-24 21:13:24 -0400

    Complete building circling logic

--- src/Character.js
+++ src/Character.js
@@ -12,12 +12,36 @@ class Character {
 		this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 		// setup motion
-		this.distance = 0;
+		this.center = {
+			x: null,
+			z: null
+		};
+		this.stopTurning();
+		this.sectionDistance = 0;
 		this.direction = 0;
+		this.zz = 0;
 	}
 
 	isTurning() {
-		return this.distance % 12 > BUILDING_WIDTH && this.distance % 12 < 12 - BUILDING_WIDTH;
+		return !!this.pivot;
+	}
+
+	startTurning() {
+		this.zz = Math.floor(this.direction + 0.2) % 4;
+		this.pivot = {
+			x: this.backing.x + BUILDING_WIDTH* (this.zz == 2 || this.zz == 3 ? -1 : 1),
+			z: this.backing.z + BUILDING_WIDTH * (this.zz == 1 || this.zz == 2 ? -1 : 1)
+		};
+		this.backing = null;
+	}
+
+	stopTurning() {
+		this.direction = Math.floor(this.direction + 0.2) % 4;
+		this.backing = {
+			x: 0,
+			z: 0
+		};
+		this.pivot = null;
 	}
 
 	update() {
@@ -26,34 +50,30 @@ class Character {
 		if (window.controller.pressed(controller.RIGHT)) offset += 0.01;
 		if (window.controller.pressed(controller.LEFT)) offset -= 0.01;
 
-		this.distance += MOVE_SPEED;
-		if (this.distance > 10.5) {
-
+		this.sectionDistance += MOVE_SPEED;
+		if (this.sectionDistance >= (this.isTurning() ? 3 : 9)) {
+			if (this.isTurning()) {
+				this.stopTurning();
+			} else {
+				this.startTurning();
+			}
+			this.sectionDistance = 0;
 		}
 
-		if (this.distance <= BUILDING_WIDTH) {
-			this.center = {
-				x: this.distance,
-				z: BUILDING_WIDTH + BUILDING_MARGIN
-			};
-		} else if (this.isTurning()) {
-			if (!this.pivot) {
-				this.pivot = {
-					x: BUILDING_WIDTH,
-					z: BUILDING_WIDTH
-				};
-			}
-			this.direction = (this.distance - BUILDING_WIDTH) / BUILDING_MARGIN / 2;
+		if (this.isTurning()) {
+			this.direction = this.zz + (this.sectionDistance) / BUILDING_MARGIN / 2;
 			this.center = {
 				x: this.pivot.x + BUILDING_MARGIN * Math.sin(this.direction * Math.PI / 2),
 				z: this.pivot.z + BUILDING_MARGIN * Math.cos(this.direction * Math.PI / 2)
 			}
-		} else if (this.distance > 12 - BUILDING_WIDTH - BUILDING_MARGIN) {
+		} else {
 			this.center = {
-				x: BUILDING_WIDTH + BUILDING_MARGIN,
-				z: BUILDING_MARGIN - BUILDING_WIDTH - this.distance + 15
+				x: (BUILDING_WIDTH + BUILDING_MARGIN) * Math.sin(this.direction * Math.PI / 2) + (this.sectionDistance - BUILDING_WIDTH) * Math.cos(this.direction * Math.PI / 2),
+				z: (BUILDING_WIDTH + BUILDING_MARGIN) * Math.cos(this.direction * Math.PI / 2) - (this.sectionDistance - BUILDING_WIDTH) * Math.sin(this.direction * Math.PI / 2)
 			}
 		}
+
+		console.log(this.direction);
 
 		this.camera.position.x = this.center.x + CAMERA_DISTANCE_BACK * Math.sin(this.direction * Math.PI / 2);
 		this.camera.position.z = this.center.z + CAMERA_DISTANCE_BACK * Math.cos(this.direction * Math.PI / 2);
@@ -62,6 +82,7 @@ class Character {
 		// this.camera.rotation.x = -1;
 		// this.camera.position.x = 5;
 		// this.camera.position.z = 6;
+		// this.model.position.y = -16;
 
 		this.model.position.x = this.center.x - offset * Math.cos(this.direction * Math.PI / 2);
 		this.model.position.z = this.center.z + offset * Math.sin(this.direction * Math.PI / 2);
